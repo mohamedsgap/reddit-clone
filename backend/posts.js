d6ff27495d96521ece3d2cc6fe9297ae5d3017e1
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-08-23 00:13:11 +0200

    feat: retrive all categories

---
+++ backend/posts.js
@@ -0,0 +1,1 @@
+const clone = require('clone')
--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,16 @@ const posts = require("./posts");
 const comments = require("./comments");
 
 const app = express();
+app.use(cors());
+
+app.get("api/categories", (req, res) => {
+  categories.getAll(req.token).then(
+    (data) => res.send(data),
+    (error) => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error",
+      });
+    }
+  );
+});
