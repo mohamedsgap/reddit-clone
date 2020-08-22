8b078cfb25cfe77268e46557a98346242cd8a6af
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-08-23 00:44:59 +0200

    feat: categories api

--- backend/categories.js
+++ backend/categories.js
@@ -1,2 +1,43 @@
 const clone = require("clone");
 const config = require("./config");
+
+let db = {};
+
+const defaultData = {
+  categories: [
+    {
+      name: "react",
+      path: "react",
+    },
+    {
+      name: "redux",
+      path: "redux",
+    },
+    {
+      name: "javascript",
+      path: "javascript",
+    },
+    {
+      name: "nodejs",
+      path: "nodejs",
+    },
+  ],
+};
+
+function getData(token) {
+  let data = db[token];
+  if (data == null) {
+    data = db[token] = clone(defaultData);
+  }
+  return data;
+}
+
+function getAll(token) {
+  return new Promise((res) => {
+    res(getData(token));
+  });
+}
+
+module.exports = {
+  getAll,
+};
