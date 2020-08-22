e643b25f8c3ed45b78b519a98f626b9573653a4d
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-08-23 01:04:54 +0200

    setup running server.js

--- backend/server.js
+++ backend/server.js
@@ -184,3 +184,20 @@ app.delete("/api/comments/:id", (req, res) => {
     }
   );
 });
+
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file
+  app.use(express.static("frontend/build"));
+
+  // Express will serve up the index.html file
+  // if doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+  });
+}
+
+app.listen(config.port, () => {
+  console.log("Server listening on port %s, Ctrl+C to stop", config.port);
+});
