3c1fc7bbf51f6b840df742fb59ce8796f5a5f9ad
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-08-23 00:57:21 +0200

    feat: posts api

--- backend/posts.js
+++ backend/posts.js
@@ -1,1 +1,172 @@
-const clone = require('clone')
+const clone = require("clone");
+
+let db = {};
+
+const defaultData = {
+  "8xf0y6ziyjabvozdd253nd": {
+    id: "8xf0y6ziyjabvozdd253nd",
+    timestamp: 1467166872634,
+    title: "When did React appear to the world",
+    body: "In March 2013.",
+    author: "thingtwo",
+    category: "react",
+    voteScore: 6,
+    deleted: false,
+    commentCount: 2,
+  },
+  "6ni6ok3ym7mf1p33lnez": {
+    id: "6ni6ok3ym7mf1p33lnez",
+    timestamp: 1468479767190,
+    title: "Learn Redux in 10 minutes!",
+    body: "Just kidding. It takes more than 10 minutes to learn technology.",
+    author: "thingone",
+    category: "redux",
+    voteScore: -5,
+    deleted: false,
+    commentCount: 0,
+  },
+  "42k3jklrjlkdfsd9fd9sdf": {
+    id: "42k3jklrjlkdfsd9fd9sdf",
+    timestamp: 1468479768222,
+    title: "Redux Core Concepts",
+    body:
+      "Redux is a predictable state container for JavaScript apps. (Not to be confused with a WordPress framework – Redux Framework.) It helps you write applications that behave consistently, run in different environments (client, server, and native), and are easy to test. On top of that, it provides a great developer experience, such as live code editing combined with a time traveling debugger.",
+    author: "Dan Abramov",
+    category: "redux",
+    voteScore: 11,
+    deleted: false,
+    commentCount: 0,
+  },
+  dfjkdfd890u38782hjh2d8: {
+    id: "dfjkdfd890u38782hjh2d8",
+    timestamp: 1468479767323,
+    title: "What's the closure in JS",
+    body:
+      "It's a combination between the function and its lexical environment, so the inner function can access and read the variables of the outer function (parent)",
+    author: "Mohamed",
+    category: "javascript",
+    voteScore: 12,
+    deleted: false,
+    commentCount: 0,
+  },
+  dfjkdfd890u38782h4drd8: {
+    id: "dfjkdfd890u38782h4drd8",
+    timestamp: 1468479769527,
+    title: "What's the best and popular framework for Node.js",
+    body: "Off course Express.JS is the right choise for any NodeJS app",
+    author: "Mike",
+    category: "nodejs",
+    voteScore: 3,
+    deleted: false,
+    commentCount: 0,
+  },
+};
+
+function getData(token) {
+  let data = db[token];
+  if (data == null) {
+    data = db[token] = clone(defaultData);
+  }
+  return data;
+}
+
+function getByCategory(token, category) {
+  return new Promise((res) => {
+    let posts = getData(token);
+    let keys = Object.keys(posts);
+    let filtered_keys = keys.filter(
+      (key) => posts[key].category === category && !posts[key].deleted
+    );
+    res(filtered_keys.map((key) => posts[key]));
+  });
+}
+
+function get(token, id) {
+  return new Promise((res) => {
+    const posts = getData(token);
+    res(posts[id].deleted ? {} : posts[id]);
+  });
+}
+
+function getAll(token) {
+  return new Promise((res) => {
+    const posts = getData(token);
+    let keys = Object.keys(posts);
+    let filtered_keys = keys.filter((key) => !posts[key].deleted);
+    res(filtered_keys.map((key) => posts[key]));
+  });
+}
+
+function add(token, post) {
+  return new Promise((res) => {
+    let posts = getData(token);
+    posts[post.id] = {
+      id: post.id,
+      timestamp: post.timestamp,
+      title: post.title,
+      body: post.body,
+      author: post.author,
+      category: post.category,
+      voteScore: 1,
+      deleted: false,
+      commentCount: 0,
+    };
+
+    res(posts[post.id]);
+  });
+}
+
+function vote(token, id, option) {
+  return new Promise((res) => {
+    let posts = getData(token);
+    post = posts[id];
+    switch (option) {
+      case "upVote":
+        post.voteScore = post.voteScore + 1;
+        break;
+      case "downVote":
+        post.voteScore = post.voteScore - 1;
+        break;
+      default:
+        console.log(`posts.vote received incorrect parameter: ${option}`);
+    }
+    res(post);
+  });
+}
+
+function disable(token, id) {
+  return new Promise((res) => {
+    let posts = getData(token);
+    posts[id].deleted = true;
+    res(posts[id]);
+  });
+}
+
+function edit(token, id, post) {
+  return new Promise((res) => {
+    let posts = getData(token);
+    for (prop in post) {
+      posts[id][prop] = post[prop];
+    }
+    res(posts[id]);
+  });
+}
+
+function incrementCommentCounter(token, id, count) {
+  const data = getData(token);
+  if (data[id]) {
+    data[id].commentCount += count;
+  }
+}
+
+module.exports = {
+  get,
+  getAll,
+  getByCategory,
+  add,
+  vote,
+  disable,
+  edit,
+  getAll,
+  incrementCommentCounter,
+};
