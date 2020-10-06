4d65be0522b1948191a5657132ecba9e8c24914d
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-10-06 16:34:37 +0200

    Add helper utils functions

---
+++ frontend/src/utils/index.js
@@ -0,0 +1,20 @@
+import _ from "lodash";
+
+export const getSortedPosts = (posts, order) => {
+  if (order === "date") {
+    return _.orderBy(posts, ["timestamp", "voteScore"], ["desc", "desc"]);
+  } else if (order === "score") {
+    return _.orderBy(posts, ["voteScore", "timestamp"], ["desc", "desc"]);
+  }
+  return posts;
+};
+
+export function dateFormatonEgypt(timestamp) {
+  return new Date(timestamp).toLocaleString("en-EG", {
+    timeZone: "Egypt/Cairo",
+  });
+}
+
+export function dateFormat(timestamp) {
+  return new Date.toString(timestamp);
+}
