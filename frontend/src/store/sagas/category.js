34707e360a728befe694d20ce97902afb6bea313
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-10-09 14:02:18 +0200

    Create categorySaga

---
+++ frontend/src/store/sagas/category.js
@@ -0,0 +1,18 @@
+import { call, put, takeLatest } from "redux-saga/effects";
+import {
+  GET_ALL_CATEGORIES,
+  GET_ALL_CATEGORIES_SUCCESS,
+} from "../actions/category";
+import { getAllCategories } from "../../utils/api";
+
+function* getAllCategoriesSaga() {
+  const categories = yield call(getAllCategories);
+  yield put({
+    type: GET_ALL_CATEGORIES_SUCCESS,
+    categories,
+  });
+}
+
+const categorySaga = [takeLatest(GET_ALL_CATEGORIES, getAllCategoriesSaga)];
+
+export default categorySaga;
