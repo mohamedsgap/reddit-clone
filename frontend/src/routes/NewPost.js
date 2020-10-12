8bb863a77ad19a982da87c682baf52aef39a48ac
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-10-12 22:26:03 +0200

    Fix NewPost component

--- frontend/src/routes/NewPost.js
+++ frontend/src/routes/NewPost.js
@@ -8,16 +8,16 @@ import { addPost } from "../store/actions";
 export default function NewPost(props) {
   const [redirect, setRedirect] = useState(false);
   const dispatch = useDispatch();
-  const addPost = (post) => {
+  const addPostFunc = (post) => {
     dispatch(addPost(post));
+    setRedirect(true);
   };
-  setRedirect(true);
   if (redirect) {
     return <Redirect to="/" />;
   }
   return (
     <div className="mt-5 container">
-      <PostForm submitPost={addPost} isNew={true} />
+      <PostForm submitPost={addPostFunc} isNew />
     </div>
   );
 }
